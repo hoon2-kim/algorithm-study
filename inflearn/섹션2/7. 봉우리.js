a37684523f4a736499a231b812ee49cd4470c57c
hoon2-kim <gooaba1204@gmail.com>
2024-08-19 18:00:16 +0900

    섹션2 문제7번 풀이

--- inflearn/섹션2/7. 봉우리.js
+++ inflearn/섹션2/7. 봉우리.js
@@ -1,18 +1,44 @@
+function solution(arr) {
+    let answer = 0;
+    let n = arr.length;
+    // 상하좌우
+    const directions = [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ];
+    let isMax = true;
 
-            function solution(arr){  
-                let answer=0;
-                let n=arr.length;
-                let dx=[-1, 0, 1, 0];
-                let dy=[0, 1, 0, -1];
-               
-                  
-                return answer;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            isMax = true;
+            for (let [dx, dy] of directions) {
+                if (
+                    i + dx >= 0 &&
+                    j + dy >= 0 &&
+                    i + dx < n &&
+                    j + dy < n &&
+                    arr[i][j] < arr[i + dx][j + dy]
+                ) {
+                    isMax = false;
+                    break;
+                }
             }
+            if (isMax) {
+                answer++;
+            }
+        }
+    }
 
-            let arr=[[5, 3, 7, 2, 3], 
-                     [3, 7, 1, 6, 1],
-                     [7, 2, 5, 3, 4],
-                     [4, 3, 6, 4, 1],
-                     [8, 7, 3, 5, 2]];
-            console.log(solution(arr));
+    return answer;
+}
 
+let arr = [
+    [5, 3, 7, 2, 3],
+    [3, 7, 1, 6, 1],
+    [7, 2, 5, 3, 4],
+    [4, 3, 6, 4, 1],
+    [8, 7, 3, 5, 2],
+];
+console.log(solution(arr));
