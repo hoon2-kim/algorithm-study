eb602268b4f76e10c183c1073df4b7623bcf838a
hoon2-kim <gooaba1204@gmail.com>
2024-09-10 23:08:20 +0900

    섹션6 문제3번 풀이

---
+++ inflearn/섹션6/3. 크레인 인형뽑기(카카오).js
@@ -0,0 +1,36 @@
+function solution(board, moves) {
+    let answer = 0;
+    let stack = [];
+
+    for (let i = 0; i < moves.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            // 변수에 저장해줘서 써야한다. 왜냐면 변수에 저장안하고 그냥 쓰면
+            // 밑에서 0으로 만들기 때문에 0으로 계속 쓰인다.
+            let doll = board[j][moves[i] - 1];
+            // 뽑는다면
+            if (doll !== 0) {
+                board[j][moves[i] - 1] = 0; // 뽑았으니 그 자리 0으로
+                if (stack.length > 0 && stack[stack.length - 1] === doll) {
+                    stack.pop();
+                    answer += 2;
+                } else {
+                    stack.push(doll);
+                }
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
+
+let a = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
